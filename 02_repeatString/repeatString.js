df9a7261e4d9d1bb649b0917a46461444564346d
Peyton Bechard <pbechard@my.yorku.ca>
2022-03-25 18:13:45 +0800

    Solves exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num < 0) return "ERROR";
+    let result = "";
+    for (num; num > 0; num--) {
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
