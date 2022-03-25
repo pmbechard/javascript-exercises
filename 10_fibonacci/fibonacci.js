3afeb838cc4277a4a71b7c9ddfde40b3294436f8
Peyton Bechard <pbechard@my.yorku.ca>
2022-03-25 20:00:12 +0800

    Completes exercises up to number 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(counter) {
+    counter = Number.parseInt(counter);
+    if (counter < 0) return "OOPS";
+    let a = 0;
+    let b = 1;
+    let current = 1;
+    for (let i = 1; i < counter - 1; i++) {
+        a = b;
+        b = current;
+        current = a + b;
+    }
+    return current;
 };
 
 // Do not edit below this line
